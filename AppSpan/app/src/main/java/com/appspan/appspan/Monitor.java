4ce728ab738cee906964f94c6ee6f239a9852fb7
Alice Black <blackfluid44@gmail.com>
2017-07-06 15:13:42 +0200

    polling

--- AppSpan/app/src/main/java/com/appspan/appspan/Monitor.java
+++ AppSpan/app/src/main/java/com/appspan/appspan/Monitor.java
@@ -11,13 +11,45 @@ import android.widget.Toast;
 import android.app.NotificationManager;
 import android.support.v4.app.NotificationCompat;
 import android.content.Context;
+import java.util.Timer;
+import java.util.TimerTask;
+import android.os.Handler;
+import android.util.Log;
 
 public class Monitor extends Service {
 
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-
         return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        final Context context=getApplicationContext();
+        Toast toast=Toast.makeText(context,"MONITOR ---- SERVICE",Toast.LENGTH_LONG);
+        toast.show();
+
+        final Handler handler = new Handler();
+        Timer timer = new Timer();
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(new Runnable() {
+                    public void run() {
+                        try {
+                            Toast toast=Toast.makeText(context,"1 minute",Toast.LENGTH_LONG);
+                            toast.show();
+                        } catch (Exception e) {
+                            Log.wtf("Exception: ", "1 minute polling failed");
+                        }
+                    }
+                });
+            }
+        };
+
+        timer.schedule(task, 0, 60*1000);  // 1 minute
     }
 }
--- AppSpan/app/src/main/java/com/appspan/appspan/MonitorBootReceiver.java
+++ AppSpan/app/src/main/java/com/appspan/appspan/MonitorBootReceiver.java
@@ -5,17 +5,16 @@ import android.content.Context;
 import android.content.Intent;
 import android.widget.Toast;
 
-public class MonitorBoot extends BroadcastReceiver {
+public class MonitorBootReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
         if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
 
-            Toast toast=Toast.makeText(context,"Monitor boot",Toast.LENGTH_LONG);
+            Toast toast=Toast.makeText(context,"Monitor boot BroadcastReceiver",Toast.LENGTH_LONG);
             toast.show();
 
             Intent monitorInten = new Intent(context, Monitor.class);
-            //monitorInten.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             context.startService(monitorInten);
         }
     }
